8c927814fdba926dfac087816bc66e963bf85565
Peter <plebedev3@gatech.edu>
2018-02-12 22:08:54 -0500

    fixed vcs

--- app/src/main/java/edu/gatech/cs2340/shelterfinder2340/controllers/LoginActivity.java
+++ app/src/main/java/edu/gatech/cs2340/shelterfinder2340/controllers/LoginActivity.java
@@ -1,8 +1,9 @@
-package edu.gatech.cs2340.shelterfinder2340;
+package edu.gatech.cs2340.shelterfinder2340.controllers;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -31,6 +32,9 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import edu.gatech.cs2340.shelterfinder2340.Login_Success;
+import edu.gatech.cs2340.shelterfinder2340.R;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
@@ -187,6 +191,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            Intent myIntent = new Intent(this, Login_Success.class);
+            if (email.equals("1234@gmail.com") && password.equals("12345")) {
+                startActivity(myIntent);
+            }
         }
     }
 
