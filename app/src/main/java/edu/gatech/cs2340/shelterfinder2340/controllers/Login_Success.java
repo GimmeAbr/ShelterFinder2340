cd58bf30272a925bdbf6cbbd80d9c46a08fcd861
Siyan Li (Sylvia) <lisiyansylvia@gmail.com>
2018-02-13 20:08:51 -0500

    Added Logout Feature. M4 Complete?

--- app/src/main/java/edu/gatech/cs2340/shelterfinder2340/controllers/Login_Success.java
+++ app/src/main/java/edu/gatech/cs2340/shelterfinder2340/controllers/Login_Success.java
@@ -1,5 +1,6 @@
 package edu.gatech.cs2340.shelterfinder2340.controllers;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,8 +23,8 @@ public class Login_Success extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent logOutIntent = new Intent(getApplicationContext(), WelcomeActivity.class);
+                startActivity(logOutIntent);
             }
         });
     }
