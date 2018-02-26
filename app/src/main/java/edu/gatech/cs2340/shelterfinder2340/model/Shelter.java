3ab15e72b46435f7cda442969cf77dc20975b36a
Mya <myaetsang@gmail.com>
2018-02-26 17:39:41 -0500

    fixed shelter model

--- app/src/main/java/edu/gatech/cs2340/shelterfinder2340/model/Shelter.java
+++ app/src/main/java/edu/gatech/cs2340/shelterfinder2340/model/Shelter.java
@@ -5,4 +5,76 @@ package edu.gatech.cs2340.shelterfinder2340.model;
  */
 
 public class Shelter {
+    private String shelterName, gender, address;
+    private int capacity, phoneNumber;
+    private double longitude, latitude;
+
+    public Shelter (String shelterName, String gender, int capacity, int phoneNumber, double longitude, double latitude) {
+        this.shelterName = shelterName;
+        this.gender = gender;
+        this.capacity = capacity;
+        this.phoneNumber = phoneNumber;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    //Setters
+    public void setShelterName(String shelterName) {
+        this.shelterName = shelterName;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+
+    //Getters
+    public String getShelterName() {
+        return shelterName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
 }
