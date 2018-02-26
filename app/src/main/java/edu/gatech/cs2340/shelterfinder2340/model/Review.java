d53dfc6427e625219db0f846b8e61a20cceedf12
Sebastian Hollister <seb.hollister@gatech.edu>
2018-02-26 17:46:06 -0500

    added Review to model

---
+++ app/src/main/java/edu/gatech/cs2340/shelterfinder2340/model/Review.java
@@ -0,0 +1,28 @@
+package edu.gatech.cs2340.shelterfinder2340.model;
+
+/**
+ * Created by Sebastian Hollister on 2/26/2018.
+ */
+
+public class Review {
+    private String shelterName;
+    public String getShelterName() { return shelterName; }
+
+    private User reviewer;
+    public User getReviewer() { return reviewer; }
+
+    private int rating;
+    public int getRating() { return rating; }
+
+    private String reviewContent;
+    public String getReviewContent() {return reviewContent; }
+
+    public Review(int rating, String reviewText, User reviewer) {
+        this.rating = rating;
+        this.reviewContent = reviewText;
+        this.reviewer = reviewer;
+    }
+
+    public void updateReviewContent(String text) {this.reviewContent = text;}
+    public void updateRating(int rating) { this.rating = rating; }
+}
