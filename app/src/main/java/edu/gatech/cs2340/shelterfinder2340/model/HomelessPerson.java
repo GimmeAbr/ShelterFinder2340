f1be6a9423773a46195f6855f40203492faca58c
Siyan Li (Sylvia) <lisiyansylvia@gmail.com>
2018-03-07 15:04:00 -0500

    Changed HomelessPerson class

--- app/src/main/java/edu/gatech/cs2340/shelterfinder2340/model/HomelessPerson.java
+++ app/src/main/java/edu/gatech/cs2340/shelterfinder2340/model/HomelessPerson.java
@@ -1,13 +1,39 @@
 package edu.gatech.cs2340.shelterfinder2340.model;
 
+import android.location.Location;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Sylvia Li on 2018/3/7.
  */
 
 public class HomelessPerson extends User {
+
+    private List<Shelter> shelterList;
+    private Location currentLocation;
     public HomelessPerson(String userName, String passWord) {
         super(userName, passWord);
+        shelterList = new ArrayList<>();
     }
+
+    public List<Shelter> getShelterList() {
+        return shelterList;
+    }
+
+    public void setShelterList(List<Shelter> shelterList) {
+        this.shelterList = shelterList;
+    }
+
+    public Location getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(Location currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+
 
     public Review submitReview(int rating, String reviewText, User reviewer) {
         return new Review(rating, reviewText, reviewer);
@@ -20,4 +46,9 @@ public class HomelessPerson extends User {
     public void updateRating(Review orReview, int rating) {
         orReview.updateRating(rating);
     }
+
+    public void markInterest(Shelter shelter) {
+        shelterList.add(shelter);
+    }
+
 }
