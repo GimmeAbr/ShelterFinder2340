b3bda6987dc80028db76035c57a399127b921fd6
Siyan Li (Sylvia) <lisiyansylvia@gmail.com>
2018-03-07 21:43:19 -0500

    Added ShelterCoordinator class

---
+++ app/src/main/java/edu/gatech/cs2340/shelterfinder2340/model/ShelterLabels.java
@@ -0,0 +1,9 @@
+package edu.gatech.cs2340.shelterfinder2340.model;
+
+/**
+ * Created by Sylvia Li on 2018/3/7.
+ */
+
+public enum ShelterLabels {
+    NAME, ADDRESS, LATITUDE, LONGITUDE, PHONENUMBER, GENDER, CAPACITY
+}
