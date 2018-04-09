44b6629aba7f565dddb3226887ff9f58954da98f
Siyan Li (Sylvia) <lisiyansylvia@gmail.com>
2018-04-09 13:01:51 -0400

    ShelterQuery class fixed and tested.

--- app/src/main/java/edu/gatech/cs2340/shelterfinder2340/model/ShelterQuery.java
+++ app/src/main/java/edu/gatech/cs2340/shelterfinder2340/model/ShelterQuery.java
@@ -1,25 +1,39 @@
 package edu.gatech.cs2340.shelterfinder2340.model;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
+ * This represents the Query object used for Filtering
  * Created by Sylvia Li on 2018/3/22.
  */
 
 public class ShelterQuery {
-    private boolean family;
-    private boolean anyone;
-    private boolean male;
-    private boolean female;
-    private boolean children;
-    private boolean young;
-    private String name;
+    private final boolean family;
+    private final boolean anyone;
+    private final boolean male;
+    private final boolean female;
+    private final boolean children;
+    private final boolean young;
+    private final String name;
 
-    private ArrayList<String> keyWord;
+    private final List<String> keyWord;
 
-    public ShelterQuery (boolean family, boolean anyone, boolean male, boolean female, boolean children, boolean young, String name){
+    /**
+     * The ShelterQuery constructor
+     *
+     * @param family   whether "family" is a criteria
+     * @param anyone   whether "anyone" is a criteria
+     * @param male     whether "male" is a criteria
+     * @param female   whether "female" is a criteria
+     * @param children whether "children" is a criteria
+     * @param young    whether "young adults" is a criteria
+     * @param name     whether a specific name is searched
+     */
+    @SuppressWarnings("ConstructorWithTooManyParameters")
+    public ShelterQuery(boolean family, boolean anyone, boolean male, boolean female,
+                        boolean children, boolean young, String name) {
         this.family = family;
         this.anyone = anyone;
         this.male = male;
@@ -51,20 +65,28 @@ public class ShelterQuery {
         }
     }
 
+    /**
+     * Filter the list of Shelters from Model
+     *
+     * @param shelterList the list of shelters (orginally)
+     * @return the filtered list
+     */
     public List<Shelter> filterShelter(List<Shelter> shelterList) {
-        List<Shelter> backupShelters = new ArrayList<Shelter>();
+        Collection<Shelter> backupShelters = new ArrayList<>();
         backupShelters.addAll(shelterList);
-        if (name.length() > 0 || male || female || children || young
+        if ((!name.isEmpty()) || male || female || children || young
                 || anyone || family) {
             for (Shelter st : backupShelters) {
-                if (!st.getShelterName().contains(name)) {
+                String shelterN = st.getShelterName();
+                if (!shelterN.contains(name)) {
                     shelterList.remove(st);
                 }
                 if (family || children || young
                         || female || male) {
                     boolean result = false;
                     for (String kw : keyWord) {
-                        result = result || st.getGender().contains(kw);
+                        String gd = st.getGender();
+                        result = result || gd.contains(kw);
                     }
                     if (!result) {
                         shelterList.remove(st);
